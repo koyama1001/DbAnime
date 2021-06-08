1d5132937d41ac5e118572a88a74c322317a50c9
koyama1001 <ryoikoyama1001@gmail.com>
2021-06-08 13:57:16 +0900

    update
    更新

--- express_mysql/app.js
+++ express_mysql/app.js
@@ -10,6 +10,7 @@ const mysql = require('mysql');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+//データベースログイン
 const con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -18,14 +19,14 @@ const con = mysql.createConnection({
 });
 
 
-
+//ログイン
 con.connect(function(err) {
 	if (err) throw err;
 	console.log('Connected');
 });
 
 
-
+//アニメデータ全て表示
 app.get('/', (req, res) => {
 	const sql = "select * from anime";
 	con.query(sql, function (err, result, fields) {  
@@ -42,10 +43,11 @@ app.post('/', (req, res) => {
 		res.redirect('/');
 	});
 });
-
+//追加機能
 app.get('/create', (req, res) => 
 	res.sendFile(path.join(__dirname, 'html/form.html')))
 
+//編集	
 app.get('/edit/:id',(req,res)=>{
 	const sql = "SELECT * FROM anime WHERE id = ?";
 	con.query(sql,[req.params.id],function (err, result, fields) {  
@@ -53,7 +55,7 @@ app.get('/edit/:id',(req,res)=>{
 		res.render('edit',{user : result});
 		});
 });
-
+//名前順に並び替える
 app.get('/name',(req,res)=>{
 	const sql = "SELECT * FROM anime  ORDER BY name ASC;";
 	con.query(sql,req.body,function(err, result, fields){
@@ -61,7 +63,7 @@ app.get('/name',(req,res)=>{
 		res.render('index',{users : result});
 		});
 	});
-
+//ID順に並び替える
 app.get('/ID',(req,res)=>{
 		const sql = "SELECT * FROM anime  ORDER BY ID ASC;";
 		con.query(sql,req.body,function(err, result, fields){
@@ -69,6 +71,8 @@ app.get('/ID',(req,res)=>{
 			res.render('index',{users : result});
 			});
 		});
+
+//評価順に昇順に並び替える
 app.get('/RANK',(req,res)=>{
 			const sql = "SELECT * FROM anime  ORDER BY rank DESC;";
 			con.query(sql,req.body,function(err, result, fields){
@@ -77,7 +81,7 @@ app.get('/RANK',(req,res)=>{
 				});
 			});
 
-
+//更新
 app.post('/update/:id',(req,res)=>{
 	const sql = "UPDATE anime SET ? WHERE id = " + req.params.id;
 	con.query(sql,req.body,function (err, result, fields) {  
@@ -86,7 +90,7 @@ app.post('/update/:id',(req,res)=>{
 		res.redirect('/');
 		});
 });
-
+//削除
 app.get('/delete/:id',(req,res)=>{
 	const sql = "DELETE FROM anime WHERE id = ?";
 	con.query(sql,[req.params.id],function(err,result,fields){
